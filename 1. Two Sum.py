080f47adef35d9af02a2468ed99f53fdc91d6f3d
Sumedh Ghavat <ghavatsumedh@gmail.com>
2024-09-13 00:38:26 -0400

    1. Two Sum

---
+++ 1. Two Sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_dict = {}
+        for i in range(len(nums)):
+            if target-nums[i] in nums_dict:
+                return [i , nums_dict[target-nums[i]]]
+            else:
+                nums_dict[nums[i]] = i
+        return []
